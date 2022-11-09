95ce0465e95ec1e7cf4acf6b6d7bc473a7b2da29
Ivan Sorroche Burgos <ivansb@MacBook-Pro-de-Ivan.local>
2022-11-09 16:09:16 -0300

    criacao da estrutura

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  console.log('chamou aqui')
+  return (
+    <>
+      <Head>
+        <title> Index ig.news</title>
+      </Head>
+      <h1>
+        Hello Word
+      </h1>
+    </>
+  )
+}
